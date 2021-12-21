99dafe955f3d8c76934e9e96528241a02b847790
RaphaelNeubert <raphi.neubert@gmail.com>
2021-12-21 09:56:58 +0100

    add nvim config

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,22 @@
+
+
+return require('packer').startup(function()
+    use 'wbthomason/packer.nvim'
+    use {'p00f/cphelper.nvim', rocks = 'http', requires = 'nvim-lua/plenary.nvim'}
+    use 'sainnhe/gruvbox-material'
+    use 'nvim-lua/popup.nvim'
+    use 'nvim-lua/plenary.nvim'
+    use 'ThePrimeagen/harpoon'
+    use {
+        'marioortizmanero/adoc-pdf-live.nvim',
+        config = "require('adoc_pdf_live').setup { \
+                    enabled = false, \
+                    viewer = 'zathura',\
+                    binary = 'asciidoctor',\
+                    params = '-r asciidoctor-pdf -b pdf',\
+                    debug = false,\
+                    style = '',\
+                    style_regex = 'style\\\\.ya?ml'\
+                }"
+    }
+end)
